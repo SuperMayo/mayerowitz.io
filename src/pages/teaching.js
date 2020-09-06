52c75219b8d7822e73b9ed033fe11da2a75a9074
Antoine Mayerowitz <antoine@mayerowitz.io>
2020-09-06 18:02:24 +0200

    Remove unused component

--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
