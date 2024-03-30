2ef30b4aa6ca5c57ff1775fee8332ecadaa0b1e6
Antoine Mayerowitz <a.mayerowitz@gmail.com>
2024-03-30 23:19:10 +0100

    chore: update configs and CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import yaml from '@rollup/plugin-yaml';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
-	plugins: [sveltekit(), yaml()],
-	optimizeDeps: {
+    plugins: [sveltekit(), yaml()],
+    optimizeDeps: {
         exclude: ["codemirror", "@codemirror/lang-cpp"],
+    },
+    ssr: {
+        noExternal: ["three"],
     },
 });
