93b00beb51361da1bfaf12d42345d896bbfb9874
Antoine Mayerowitz <a.mayerowitz@gmail.com>
2024-04-16 19:36:32 +0200

    feat: Add links to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,6 +2,13 @@
     let year = new Date().getFullYear();
 </script>
 
-<footer class="text-center shrink-0 p-one mt-auto">
+<footer class="mt-auto shrink-0 p-one text-center">
     © {year}{` `}Antoine Mayerowitz
+    <div class="[&>a:hover]:opacity-100 [&>a]:opacity-50">
+        <a href="/feed">RSS</a> &#8226;
+        <a href="https://stats.sender.net/forms/aARmBb/view">Newsletter</a>
+        &#8226;
+        <a href="https://ko-fi.com/antoinemayerowitz">Support me</a>
+    </div>
 </footer>
+
