858460cd32d9f380815ddd66acca2b149bad9580
Antoine Mayerowitz <a.mayerowitz@gmail.com>
2025-09-09 17:53:50 +0200

    ✨ New article: By the way, what are AA?

--- src/lib/Types.ts
+++ src/lib/Types.ts
@@ -1,20 +1,23 @@
 export type Categories = "sveltekit" | "svelte";
 
-export type BasePost = {
+export type CommonPostFields = {
     title: string;
     slug: string;
     subtitle: string;
     description: string;
     date: string;
     published: boolean;
-    external: false;
     standalone: boolean;
     SEO_description: string;
 };
 
-export type ExternalPost = BasePost & {
+export type InternalPost = CommonPostFields & {
+    external: false;
+};
+
+export type ExternalPost = CommonPostFields & {
     external: true;
     externalUrl: string;
 };
 
-export type Post = BasePost | ExternalPost;
+export type Post = InternalPost | ExternalPost;
---
+++ src/routes/(app)/blog/substack-what-are-aa/+page.md
@@ -0,0 +1,13 @@
+---
+title: "By the way, what are AA?"
+external: true
+externalUrl: "https://hushcrasher.substack.com/p/taxonomy-of-games"
+date: "2025-09-09"
+readingTime: 8
+category: "video games"
+tags: ["video games", "business", "data science"]
+published: true
+description: "A data-driven taxonomy of games scope. (On Substack)"
+SEO_description: |
+    Instead of fuzzy labels like 'indie' and 'AA', I use a data-driven approach to classify video games based based on their actual scope.
+---
